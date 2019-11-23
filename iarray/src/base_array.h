9cb52178e4cb7124be1f233330402e7ec01f3bfe
Serge Feiler <srgmatveev@gmail.com>
2019-11-23 22:06:50 +0300

    add step base array add push

--- iarray/src/base_array.h
+++ iarray/src/base_array.h
@@ -14,21 +14,25 @@
 template<typename T>
 struct node {
     T val;
+
     node() = default;
+
     node(const T &val) : val(val) {};
 
     node &operator=(node *rht) {
         this->val = rht->val;
-        std::cout<<"worked"<<std::endl;
         return *this;
     }
 
     friend std::ostream &operator<<(std::ostream &out, const node &node_lnk) {
-        out << node_lnk;
+        out << node_lnk.val;
         return out;
     }
 
-
+    friend std::ostream &operator<<(std::ostream &out, const node *node_lnk) {
+        out << node_lnk->val;
+        return out;
+    }
 };
 
 template<class T, class X, typename = std::enable_if_t<std::is_unsigned<X>::value>>
@@ -53,48 +57,77 @@ public:
     BaseArray(const X &x = FULL_SIZE) : full_size(x), n{0}, tmp_index{0} { init(full_size); }
 
     ~BaseArray() {
-        for(auto i=0; i< full_size;++i){
-            if(((size_t*)arr)[i]) {
-                node<T>*t = (node<T>*) *((size_t*)arr+i);
-                if(t) {
-                    for(auto j=i+1;j<full_size;++j){
-                        node<T>*t1 = (node<T>*) *((size_t*)arr+j);
-                        if(t1 == t) {*((size_t*)arr+j)=NULL;}
+        for (auto i = 0; i < full_size; ++i) {
+            if (((size_t *) arr)[i]) {
+                node<T> *t = (node<T> *) *((size_t *) arr + i);
+                if (t) {
+                    for (auto j = i + 1; j < full_size; ++j) {
+                        node<T> *t1 = (node<T> *) *((size_t *) arr + j);
+                        if (t1 == t) { *((size_t *) arr + j) = 0; }
                     }
-                    delete(t);
+                    delete (t);
                 }
             }
         }
         if (arr) free(arr);
     }
 
-    BaseArray &operator=(node<T> **r_node) {
-        std::cout << "equal = " << (*r_node)->val << std::endl;
+    operator node<T> *() {
+        return (node<T> *) *((size_t *) arr + tmp_index);
+    }
 
-        size_t* tmp = (size_t*)*r_node;
-        ((size_t*)arr)[tmp_index] = (size_t)std::addressof(**r_node);
-        std::cout << "address=" << *r_node <<" "<<tmp<<std::endl;
-        std::cout << "address=" << *r_node <<" "<<std::hex<<((size_t*)arr)[tmp_index]<<std::endl;
+    BaseArray &operator=(node<T> **r_node) {
+        size_t *tmp = (size_t *) *r_node;
+        node<T> *t = (node<T> *) *((size_t *) arr + tmp_index);
+        node<T> *t1 = nullptr;
+        bool del = true;
+        if (t) {
+            for (auto i = 0; i < full_size; ++i) {
+                if (i != tmp_index) {
+                    t1 = (node<T> *) *((size_t *) arr + i);
+                    if (t1 == t) {
+                        del = false;
+                        break;
+                    }
+                }
+            }
+            if (del) {
+                delete (t);
+                *((size_t *) arr + tmp_index) = 0;
+            }
+
+        }
+        ((size_t *) arr)[tmp_index] = (size_t) std::addressof(**r_node);
         return *this;
     }
 
     BaseArray &operator[](const X index) {
         if (index > full_size - 1) std::range_error("out of index");
-        std::cout << "index = " << index << std::endl;
+        if (n < index) n = index + 1;
         this->tmp_index = index;
-
         return *this;
     }
 
-
-    friend std::ostream &operator<<(std::ostream &out, const BaseArray &baseArray) {
-        if(((size_t*)baseArray.arr)[baseArray.tmp_index]) {
-            out<<std::dec<<((node<T>**)baseArray.arr)[baseArray.tmp_index]->val;
+    bool push_back(node<T> *t) {
+        if ((n + 1) >= full_size) {
+            n = full_size - 1;
+            return false;
         }
-
-         return out;
+        ((size_t *) arr)[tmp_index] = (size_t) std::addressof(*t);
+        n++;
+        tmp_index = n - 1;
+        std::cout << n << " " << tmp_index << " " << std::endl;
+        std::cout << t << " address = " << std::hex<<((size_t*)arr)[0] << std::endl;
+        return true;
     }
 
+    friend std::ostream &operator<<(std::ostream &out, const BaseArray &baseArray) {
+        if (((size_t *) baseArray.arr)[baseArray.tmp_index]) {
+            out << ((node<T> **) baseArray.arr)[baseArray.tmp_index]->val;
+        }
+
+        return out;
+    }
 };
 
 
--- iarray/test/test_base_array.cpp
+++ iarray/test/test_base_array.cpp
@@ -17,17 +17,59 @@ protected:
         if (baseArray) delete (baseArray);
     }
 };
-
-TEST_F(BaseArray_Test, one_element) {
+/*
+TEST_F(BaseArray_Test, as_array_element) {
+    std::string output;
     node<int> *node1 = new node<int>(4);
     node<int> *node2 = new node<int>(26);
+    node<int> *node3 = new node<int>(18);
+    node<int> *node5 = new node<int>(28);
     (*baseArray)[0] = &node1;
     (*baseArray)[1] = &node2;
     (*baseArray)[2] = &node2;
-    std::cout << (*baseArray)[0] << std::endl;
-    std::cout << (*baseArray)[1] << std::endl;
-    std::cout << (*baseArray)[5] << std::endl;
+    testing::internal::CaptureStdout();
+    std::cout << (*baseArray)[0];
+    output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, "4");
+    testing::internal::CaptureStdout();
+    std::cout << (*baseArray)[1];
+    output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, "26");
+    testing::internal::CaptureStdout();
+    std::cout << (*baseArray)[5];
+    output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, "");
+    node<int> *node4 = (*baseArray)[2];
+    testing::internal::CaptureStdout();
+    std::cout << node4;
+    output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, "26");
 
-    //delete (node1);
-    //delete (node2);
+    testing::internal::CaptureStdout();
+    (*baseArray)[2] = &node3;
+    std::cout << (*baseArray)[2];
+    output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, "18");
+
+    testing::internal::CaptureStdout();
+    (*baseArray)[2] = &node5;
+    std::cout << (*baseArray)[2];
+    output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, "28");
+
+
+}
+*/
+TEST_F(BaseArray_Test, as_push_pop_element) {
+    std::cout<<std::endl;
+    node<int> *node1 = new node<int>(88);
+    //node<int> *node2 = new node<int>(18);
+    baseArray->push_back(node1);
+    baseArray->push_back(node1);
+    baseArray->push_back(node1);
+    baseArray->push_back(node1);
+    //for(auto i = 0; i< 3; ++i)
+    std::cout<<(*baseArray)[0]<<std::endl;
+    std::cout<<(*baseArray)[1]<<std::endl;
+    std::cout<<(*baseArray)[2]<<std::endl;
 }
